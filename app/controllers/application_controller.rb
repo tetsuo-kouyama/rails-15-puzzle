5e268c66722370ddc0109d4acc7de9ed3d7e2e46
tetsuo-kouyama <kouyamatetuo1129@gmail.com>
2026-03-10 14:03:06 +0900

    feat: add header css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
   # before_action :require_login
+
+  def logged_in?
+    current_user.present?
+  end
 
   private
 
