3a9758990cd3dbb4013857951f9fc6a2813fc35a
tetsuo-kouyama <kouyamatetuo1129@gmail.com>
2026-03-19 18:12:04 +0900

    refactor: controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
-  # skip_before_action :require_login, only: %i[new create]
-
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-
 
     if @user.save
       session[:user_id] = @user.id
